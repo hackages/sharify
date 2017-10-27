5bfd815e8ac9ac8ef1ff53c5ac4d3b4eab23e35d
Antonio Calapez <antonio@hackages.io>
2017-10-27 09:20:54 +0200

    Adding exercices

---
+++ 05_arrays/10_first.js
@@ -0,0 +1,14 @@
+const tab1 = ['Hello', 'je', 'suis', 'un', 'tableau'];
+const tab2 = [2, 4, 5];
+const tab3 = [true, 0, false];
+
+/**
+ * Retourner le premier élément du tableau que l'on vous donne en paramètre. 
+ */
+function first(tableau){
+
+}
+
+console.log(first(tab1)); // Expected Output: 'Hello'
+console.log(first(tab2)); // Expected Output: 2
+console.log(first(tab3)); // Expected Output: true
---
+++ 05_arrays/11_last.js
@@ -0,0 +1,14 @@
+const tab1 = ['Hello', 'je', 'suis', 'un', 'tableau'];
+const tab2 = [2, 4, 5];
+const tab3 = [true, 0, false];
+
+/**
+ * Retourner le premier élément du tableau que l'on vous donne en paramètre. 
+ */
+function last(tableau){
+
+}
+
+console.log(last(tab1)); // Expected Output: 'tableau'
+console.log(last(tab2)); // Expected Output: 5
+console.log(last(tab3)); // Expected Output: false
